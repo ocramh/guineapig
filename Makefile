9e4838ab635f4b34193aa4af415450dc60bbc799
Popcore <smallzed@gmail.com>
2019-04-23 14:25:37 +0100

    add database service

--- Makefile
+++ Makefile
@@ -1,7 +1,19 @@
-.PHONY: docker_build
-docker_build:
-	docker build --tag ocramh/guineapig .
+.PHONY: docker_build_app
+docker_build_app:
+	docker build --tag popcore/guineapig:v2.1 --file=docker/Dockerfile.app .
 
-.PHONY: docker_run
-docker_run:
-	docker run --rm -d --name guineapig -p 8080:8080 ocramh/guineapig lateste
+.PHONY: docker_build_db
+docker_build_db:
+	docker build --tag popcore/guineapig_db:v1 --file=docker/Dockerfile.db .
+
+.PHONY: docker_run_app
+docker_run_app:
+	docker run --rm -d --name guineapig -p 8080:8080 popcore/guineapig v2.1
+
+.PHONY: docker_run_db
+docker_run_db:
+	docker run --rm -d --name guineapig_db -p 5432:5432 popcore/guineapig_db:v1
+
+.PHONY: docker_psql
+docker_psql:
+	docker run -it --link guineapig_db:postgres popcore/guineapig_db:v1 psql -h postgres -d postgres -U user
--- main.go
+++ main.go
@@ -1,16 +1,61 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	defer func(start time.Time) {
+		log.Printf("/ handler. status: ok. duration: %v", time.Now().Sub(start))
+	}(time.Now())
+
 	fmt.Fprintf(w, "hello world")
+}
+
+func dbHandler(w http.ResponseWriter, r *http.Request) {
+	defer func(start time.Time) {
+		log.Printf("/db handler. status: ok. duration: %v", time.Now().Sub(start))
+	}(time.Now())
+
+	host := os.Getenv("GUINEAPIG_DB_SERVICE_HOST")
+	user := os.Getenv("SECRET_USER")
+	password := os.Getenv("SECRET_PASSWORD")
+	dbName := os.Getenv("SECRET_DBNAME")
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", host, user, password, dbName)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := db.Query("SELECT * FROM test LIMIT 1;")
+	if err != nil {
+		log.Println(err)
+	}
+	defer rows.Close()
+
+	var out []string
+	for rows.Next() {
+		var n string
+		if err := rows.Scan(&n); err != nil {
+			log.Println(err)
+		}
+
+		out = append(out, n)
+	}
+
+	fmt.Fprintf(w, out[0])
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/db", dbHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
